87aa3eabaa0fc7f0e6a2f272b800d608d5a082e0
conjectures <alexis.othonos@gmail.com>
2022-05-23 07:14:30 +0100

    testing secrets version

--- main.tf
+++ main.tf
@@ -6,29 +6,44 @@ provider "google" {
 
 # Create network
 resource "google_compute_network" "vpc_network" {
-    name = "vpc-network"
+    name = "terraform-network"
 }
 
+
+output "something" {
+    value = data.google_secret_manager_secret_version.my-secret.secret_data
+    sensitive = true
+}
 
 # Create metadata repo
 resource "google_sql_database_instance" "main" {
     name = "metadata-main"
-    database_version = "SQLSERVER_2017_EXPRESS"
-    region = "europe-west2"
+    database_version = "SQLSERVER_2019_EXPRESS"
     deletion_protection = "false"
+    root_password = data.google_secret_manager_secret_version.my-secret.secret_data
     settings {
-        tier = "db-f1-micro"
+        tier = "db-custom-1-3840"
         # ip_configuration
     }
 }
 
+# Specify 'google_secret_manager_secret_version' as DATA SOURCE 
 data "google_secret_manager_secret_version" "my-secret" {
     provider = google
     secret = "my-secret"
-    version = "1"
+    # Ignore version to use latest
+    #version = "1"
 }
 
-output "something" {
-    value = data.google_secret_manager_secret_version.my-secret.secret_data
-    sensitive = false
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "e2-micro"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-9"
+        }
+    }
+    network = google_compute_network.vpc_network.self_link
 }
